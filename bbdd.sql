7d4399d9f6f45caddc6e5b12b5522e7b6c972d9d
Miguel Chacón Carrasco <michaca94@gmail.com>
2018-05-24 12:54:19 +0200

    bbdd y licores

---
+++ bbdd.sql
@@ -0,0 +1,73 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `usuario` (
+  `nombre_usuario` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `password` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish_ci NOT NULL )
+   ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`nombre_usuario`);
+
+  INSERT INTO `data_users` (`nombre_usuario`, `password`, `nombre`, `email`, `tipo`) VALUES
+('miguel', '1234', 'Miguel Chacon', 'miguel@prueba.com', 'administrador');
+
+
+CREATE TABLE `cerveza` (
+  `id_cerveza` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+ `lugar` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `opinion` varchar(255) COLLATE utf8_spanish_ci 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+ALTER TABLE `cerveza`
+  ADD PRIMARY KEY (`id_cerveza`);
+
+CREATE TABLE `vino` (
+  `id_vino` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `year` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+ `lugar` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `opinion` varchar(255) COLLATE utf8_spanish_ci 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+ALTER TABLE `vino`
+  ADD PRIMARY KEY (`id_vino`);
+
+ CREATE TABLE `licor` (
+  `id_licor` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `clasifiocacion` varchar(30) COLLATE utf8_spanish_ci,
+ `lugar` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `opinion` varchar(255) COLLATE utf8_spanish_ci ,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+ ALTER TABLE `licor`
+  ADD PRIMARY KEY (`id_licor`);
+
+   CREATE TABLE `otro` (
+  `id_otro` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `nombre` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `tipo` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `subtipo` varchar(30) COLLATE utf8_spanish_ci,
+  `opinion` varchar(255) COLLATE utf8_spanish_ci 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+    ALTER TABLE `otro`
+  ADD PRIMARY KEY (`id_otro`);
+
+
+ CREATE TABLE `comentario` (
+  `id` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `comentario` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
+  `id_usuario` varchar(30) COLLATE utf8_spanish_ci NOT NULL,
+  `id_bebida` varchar(30) COLLATE utf8_spanish_ci,
+  `tipo_bebida` varchar(30) COLLATE utf8_spanish_ci 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+    ALTER TABLE `otro`
+  ADD PRIMARY KEY (`id_otro`);
